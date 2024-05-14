70d04268acad41cd5ded2b9930b3b3e0a01e6bb3
andydobre0 <dobreandy1@gmail.com>
2024-05-14 11:45:44 +0300

    FIRST DEPLOY AZURE

--- Program.cs
+++ Program.cs
@@ -80,11 +80,11 @@ builder.Services.AddAuthentication(options =>
 });
 builder.Services.AddAuthorization();
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
