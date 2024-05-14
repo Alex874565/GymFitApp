b65afaeee2e1781ac1a164e5383f8c4c3be914c7
Alex874565 <alex.carpinisan@gmail.com>
2024-05-14 11:55:51 +0300

    Added Register

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GymFit.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using static GymFit.Controllers.LoginController;
 
 namespace GymFit.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class RegisterController : Controller
     {
-        public IActionResult Index()
+
+        private IConfiguration _config;
+        GymFitContext db = new GymFitContext();
+
+        public RegisterController(IConfiguration config)
         {
-            return View();
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IResult Register([FromBody] UserData user_data)
+        {
+            var users = from client in db.Clients
+                          where client.Email.Equals(user_data.email)
+                          select client;
+            if (users.Count() != 0)
+            {
+                Client user = new Client();
+                user.Email = user_data.email;
+                user.Password = user_data.password;
+                user.Role = "Client";
+                db.Clients.Add(user);
+                db.SaveChangesAsync();
+                return Results.Ok(user);
+            }
+            return Results.StatusCode(401);
         }
     }
 }
+
