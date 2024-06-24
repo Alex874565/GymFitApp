cabadc2fdf4b7bd6407edffc5f3b771942d5df83
popescuadi15 <100568629+popescuadi15@users.noreply.github.com>
2024-06-24 17:55:29 +0300

    Orice

--- Frontend/src/Layout/User/CourseCard.jsx
+++ Frontend/src/Layout/User/CourseCard.jsx
@@ -5,8 +5,8 @@ const CourseCard = ({ course }) => {
   return (
     <div className="bg-white p-6 rounded-lg shadow-lg">
       <h3 className="text-xl font-bold mb-2">{course.name}</h3>
-      <p className="text-gray-700 mb-2">Description: {course.description}</p>
-      <p className="text-gray-700 mb-2">Duration: {course.duration} minutes</p>
+      <p className="text-gray-700 mb-2">Description: {course.Description}</p>
+      <p className="text-gray-700 mb-2">Duration: {course.Duration} minutes</p>
       <Link
         to={`/course-schedule/${course.id}`}
         className="text-blue-500 hover:underline"
